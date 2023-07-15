6ad0e137e7508755ad0b32c2928b8087401a808c
2dogsandaguy <Neverclear@ymail.com>
2023-07-15 12:34:56 -0400

    coded the random fuction to find numbers, letters, and symbols

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password =
 
 // Write password to the #password input
 function writePassword() {
@@ -13,11 +14,41 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword ) 
+
+
+  
+
+
+var listitems  ={
+    lower: getlowercase,
+    upper: getuppercase,
+    number: getnumber,
+    symbol: getsymbol
+};
+
 
 function getlowercase() {
-  return string.fromCharCode(Math.floor(math.random() * 26) + 97)
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getuppercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 
-console. log(getlowercase() );
+function getnumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getsymbol() {
+    return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+}
+console.log(getlowercase());
+
+console.log(getuppercase());
+
+console.log(getnumber());
+
+console.log(getsymbol());
+
